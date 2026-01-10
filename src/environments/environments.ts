b9482384dc6c783419e6ebf3a351427cbb443013
devasrichinalachi <devasrisudhama@gmail.com>
2026-01-10 15:14:15 +0530

    fixed api

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api'  // Local backend
+  production: true,
+  apiUrl: 'https://rsnbe.vercel.app/'  // Local backend
 };
